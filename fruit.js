f8092ea13966a673d184eaa3ba871c43e561d3b9
Zach Garman <zachgarman@Zachs-MacBook-Pro.local>
2016-09-29 23:55:17 -0500

    Clean HTML and JS, add styling

--- fruit.js
+++ fruit.js
@@ -1,169 +1,73 @@
 var money = 100;
-var apple = {
-	quantity: 0,
-	spentOn: 0,
-	currentPrice: 5,
-	totalBought: 0,
-	soldFor: 0,
-	getProfit: function() {
-		return spentOn - soldFor;
-	},
-	getAvg: function() {
-		if (this.totalBought === 0) {
-			return 0;
-		} else {
-			var x = this.spentOn / this.totalBought;
-			return x.toFixed(2);
-		}
-	},
-	getAvgSold: function() {
-		if (this.totalBought === 0) {
-			return 0;
-		} else {
-			var x = this.soldFor / (this.totalBought-this.quantity);
-			return x.toFixed(2);
-		}
-	}
-};
-var orange = {
-	quantity: 0,
-	spentOn: 0,
-	currentPrice: 5,
-	totalBought: 0,
-	soldFor: 0,
-	getProfit: function() {
-		return spentOn - soldFor;
-	},
-	getAvg: function() {
-		if (this.totalBought === 0) {
-			return 0;
-		} else {
-			var x = this.spentOn / this.totalBought;
-			return x.toFixed(2);
-		}
-	},
-	getAvgSold: function() {
-		if (this.totalBought === 0) {
-			return 0;
-		} else {
-			var x = this.soldFor / (this.totalBought-this.quantity);
-			return x.toFixed(2);
-		}
-	}
-};
-var banana = {
-	quantity: 0,
-	spentOn: 0,
-	currentPrice: 5,
-	soldFor: 0,
-	totalBought: 0,
-	getProfit: function() {
-		return spentOn - soldFor;
-	},
-	getAvg: function() {
-		if (this.totalBought === 0) {
-			return 0;
-		} else {
-			var x = this.spentOn / this.totalBought;
-			return x.toFixed(2);
-		}
-	},
-	getAvgSold: function() {
-		if (this.totalBought === 0) {
-			return 0;
-		} else {
-			var x = this.soldFor / (this.totalBought-this.quantity);
-			return x.toFixed(2);
-		}
-	}
-};
-var grape = {
-	quantity: 0,
-	spentOn: 0,
-	currentPrice: 5,
-	totalBought: 0,
-	soldFor: 0,
-	getProfit: function() {
-		return spentOn - soldFor;
-	},
-	getAvg: function() {
-		if (this.totalBought === 0) {
-			return 0;
-		} else {
-			var x = this.spentOn / this.totalBought;
-			return x.toFixed(2);
-		}
-	},
-	getAvgSold: function() {
-		if (this.totalBought === 0) {
-			return 0;
-		} else {
-			var x = this.soldFor / (this.totalBought-this.quantity);
-			return x.toFixed(2);
-		}
-	}
-};
-//Checks to see what fruit we have and appends relative data
-function checkFruit($obj) {
-	console.log($obj.attr('class'));
-	switch ($obj.attr('class')) {
-		case 'apple':
-		$('#apple_avg').empty();
-		$('#apple_amount').empty();
-		$('#apple_avg').append(buyFruit(apple));
-		$('#apple_amount').append(apple.quantity);
-		break;
-		case 'orange':
-		$('#orange_avg').empty();
-		$('#orange_amount').empty();
-		$('#orange_avg').append(buyFruit(orange));
-		$('#orange_amount').append(orange.quantity);
-		break;
-		case 'banana':
-		$('#banana_avg').empty();
-		$('#banana_amount').empty();
-		$('#banana_avg').append(buyFruit(banana));
-		$('#banana_amount').append(banana.quantity);
-		break;
-		case 'grape':
-		$('#grape_avg').empty();
-		$('#grape_amount').empty();
-		$('#grape_avg').append(buyFruit(grape));
-		$('#grape_amount').append(grape.quantity);
-		break;
-	}
-}
+var startingPrice = 5;
+var apple = new Fruit('apple');
+var orange = new Fruit('orange');
+var banana = new Fruit('banana');
+var grape = new Fruit('grape');
+var allFruit = [apple, orange, banana, grape];
 
+//constructor function to start a new fruit object for each type.
+function Fruit (name) {
+	this.name = name;
+	this.quantity = 0;
+	this.spentOn = 0;
+	this.currentPrice = startingPrice;
+	this.totalBought = 0;
+	this.totalSold = 0;
+	this.soldFor = 0;
+	this.getProfit = function () {
+		return (this.soldFor - this.spentOn).toFixed(2);
+	};
+	this.getAvg = function () {
+		if(this.totalBought === 0) {
+			return 0;
+		} else {
+			var avg = this.spentOn / this.totalBought;
+			return avg.toFixed(2);
+		}
+	};
+	this.getAvgSold = function () {
+		if (this.totalSold === 0) {
+			return 0;
+		} else {
+			var avg = this.soldFor / this.totalSold;
+			return avg.toFixed(2);
+		}
+	};
+}
+//This function converts the clicked button to its corresponding Fruit object
+//It then passes on that fruit object to update the DOM for a purchase
+function checkFruit($obj) {
+	// Find the name of the fruit, which is the same as the class
+	var name = $obj.attr('class');
+	//find the actual object to pass on.
+	var $fruit = fruitObj($obj);
+	$('#' + name + '_avg').text('$' + buyFruit($fruit));
+	$('#' + name + '_amount').text($fruit.quantity);
+	$('#' + name + '_profit').text('$' + $fruit.getProfit());
+}
+//This function converts the clicked button to its corresponding Fruit object
+//It then passes on that fruit object to update the DOM for a sale
 function checkFruitSell($obj) {
-	console.log($obj.attr('class'));
-	switch ($obj.attr('class')) {
-		case 'apple':
-		$('#apple_avgSold').empty();
-		$('#apple_amount').empty();
-		$('#apple_avgSold').append(sellFruit(apple));
-		$('#apple_amount').append(apple.quantity);
-		break;
-		case 'orange':
-		$('#orange_avgSold').empty();
-		$('#orange_amount').empty();
-		$('#orange_avgSold').append(sellFruit(orange));
-		$('#orange_amount').append(orange.quantity);
-		break;
-		case 'banana':
-		$('#banana_avgSold').empty();
-		$('#banana_amount').empty();
-		$('#banana_avgSold').append(sellFruit(banana));
-		$('#banana_amount').append(banana.quantity);
-		break;
-		case 'grape':
-		$('#grape_avgSold').empty();
-		$('#grape_amount').empty();
-		$('#grape_avgSold').append(sellFruit(grape));
-		$('#grape_amount').append(grape.quantity);
-		break;
+	// Find the name of the fruit, which is the same as the class
+	var name = $obj.attr('class');
+	//find the actual object to pass on.
+	var $fruit = fruitObj($obj);
+	$('#' + name + '_avgSold').text('$' + sellFruit($fruit));
+	$('#' + name + '_amount').text($fruit.quantity);
+	$('#' + name + '_profit').text('$' + $fruit.getProfit());
+}
+//This function will take a button element that was clicked and return
+//the actual object for the fruit so that we can pass it on to other functions.
+function fruitObj ($button) {
+	for (var i = 0; i < allFruit.length; i++) {
+		if ($button.attr('class') === allFruit[i].name) {
+			return allFruit[i];
+		}
 	}
 }
-// Checks to see if we can buy. Updates to new quantity if we can and returns new avg
+//Checks to see if there is money on hand to afford the fruit.
+//Updates all Fruit Object Properties and the DOM to reflect the purchase
 function buyFruit(obj) {
 	if (obj.currentPrice > money) {
 		return obj.getAvg();
@@ -172,25 +76,26 @@ function buyFruit(obj) {
 		obj.totalBought++;
 		money -= obj.currentPrice;
 		obj.spentOn += obj.currentPrice;
-		$('h2 > span').empty();
-		$('h2 > span').append(money.toFixed(2));
+		$('h2 > span').text('$' + money.toFixed(2));
 		return obj.getAvg();
 	}
 }
-
+//Checks to see if there is fruit on hand to sell,
+//Updates all Fruit object properties and the DOM to reflect the sale.
 function sellFruit(obj) {
 	if (obj.quantity <= 0) {
-		return obj.getAvg();
+		return obj.getAvgSold();
 	} else {
 		obj.quantity--;
+		obj.totalSold++;
 		money += obj.currentPrice;
 		obj.soldFor += obj.currentPrice;
-		$('h2 > span').empty();
-		$('h2 > span').append(money.toFixed(2));
+		$('h2 > span').text('$' + money.toFixed(2));
 		return obj.getAvgSold();
 	}
 }
-
+//Randomly increments the price of a fruit between -$.50 and $.50
+//Also caps the price so that it won't exceed 9.99 or go below .50.
 function getRandomPrice(obj) {
 	var num = randomNumber() / 100;
 	obj.currentPrice += num;
@@ -200,69 +105,73 @@ function getRandomPrice(obj) {
 		obj.currentPrice = .50;
 	}
 }
-
+//Randomly generates a new number between -50 and 50;
 function randomNumber() {
 	return Math.floor(Math.random() * (101)) - 50;
 }
-
+//Cycles through each fruit object and randomly adjusts price.
+//Updates the DOM to display the new price.
 function setPrice() {
-	getRandomPrice(apple);
-	getRandomPrice(orange);
-	getRandomPrice(banana);
-	getRandomPrice(grape);
-	$('#apple_price').empty();
-	$('#orange_price').empty();
-	$('#banana_price').empty();
-	$('#grape_price').empty();
-	$('#apple_price').append(apple.currentPrice.toFixed(2));
-	$('#orange_price').append(orange.currentPrice.toFixed(2));
-	$('#banana_price').append(banana.currentPrice.toFixed(2));
-	$('#grape_price').append(grape.currentPrice.toFixed(2));
+	allFruit.forEach(function(obj) {
+		getRandomPrice(obj);
+		var price = obj.currentPrice.toFixed(2);
+		var objPrice = '#' + obj.name + '_price';
+		$(objPrice).text('$' + price);
+	});
 }
+//Function used to sell off all remaining quantities on hand for each Fruit
+//object and return the total sale proceeds
+function yardSale () {
+	var proceeds = 0;
+	allFruit.forEach(function (fruit) {
+		proceeds += (fruit.quantity * fruit.currentPrice);
+		fruit.totalSold += fruit.quantity;
+		fruit.soldFor += (fruit.quantity * fruit.currentPrice);
+		fruit.quantity = 0;
+	});
+	return proceeds;
+}
+
 $(document).ready(function() {
 	setPrice();
-	var interval = setInterval(setPrice, 300);
-	// $('.applebuy').on('click', 'button', function(){
-	// 	$('#apple_avg').empty();
-	// 	$('#apple_amount').empty();
-	// 	$('#apple_avg').append(buyFruit(apple));
-	// 	$('#apple_amount').append(apple.quantity);
-	// })
+	//Function to run setPrice every 0.5 seconds;
+	var interval = setInterval(setPrice, 500);
+	//Event Listener for a click on a buy button
 	$('#buy').on('click', 'button', function() {
 		checkFruit($(this));
 	})
+	//Event Listener for a click on a sell button
 	$('#sell').on('click', 'button', function() {
 		checkFruitSell($(this));
 	})
+	//Timer set to stop the interval after 30 seconds and compile all data
+	//Also stops all future play in the game.
 	setTimeout(function(){
+		//Stop the price changes from happening.
 		clearInterval(interval);
-		var appleMoney = apple.quantity * apple.currentPrice;
-		var orangeMoney = orange.quantity * orange.currentPrice;
-		var bananaMoney = banana.quantity * banana.currentPrice;
-		var grapeMoney = grape.quantity * grape.currentPrice;
-		money = appleMoney + orangeMoney + bananaMoney + grapeMoney + money;
-		console.log(money);
-		alert('You made $' + money.toFixed(2));
-		money = 0;
-		apple.quantity = 0;
-		orange.quantity = 0;
-		banana.quantity = 0;
-		grape.quantity = 0;
+		//run yardSale to sell off any fruit still on hand at the last market price
+		// and adds that total to money
+		money += yardSale();
 
-}, 30000);
+		//Append DOM to show final totals
+		$('h2 > span').text('$' + money.toFixed(2))
+		allFruit.forEach(function (fruit) {
+			var name = fruit.name;
+			$('#' + name + '_avgSold').text('$' + fruit.getAvgSold());
+			$('#' + name + '_amount').text('0');
+			$('#' + name + '_profit').text('$' + fruit.getProfit());
+		});
 
-
+		$('#sell').off();
+		$('#buy').off();
+		var message;
+		if (money < 100) {
+			var total = 100 - money;
+			message = 'Bad job!  You lost $' + total.toFixed(2) + '.';
+		} else {
+			var total = money - 100;
+			message = 'Nice work! You made $' + total.toFixed(2) + '.';
+		}
+		alert(message);
+	}, 30000);
 });
-
-// var apples = 0;
-// var oranges = 0;
-// var bananas = 0;
-// var grapes = 0;
-// var totalSpentApples = 0;
-// var totalSpentOranges = 0;
-// var totalSpentBananas = 0;
-// var totalSpentGrapes = 0;
-// var currentApplePrice = 5;
-// var currentOrangePrice = 5;
-// var currentBananaPrice = 5;
-// var currentGrapePrice = 5;
